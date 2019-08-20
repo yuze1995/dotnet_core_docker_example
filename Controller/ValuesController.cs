d08b7a0ff99afa19a8ef8dc3dd2fc610c8df376b
YUZE1995\yuze1 <zxc24630918@gmail.com>
2019-08-20 23:19:14 +0800

    init

---
+++ Controller/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreDockerExample.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("First")]
+        public object GetFirst()
+        {
+            return new { value = 1 };
+        }
+    }
+}
